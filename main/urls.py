3caa1df0ed34eac879407f5bfba05d329c906819
elizabeth <elizabethmeilanny@gmail.com>
2025-09-16 22:34:01 +0700

    Tugas 3 complete

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,6 @@ from main.views import show_main, create_product, show_product, show_xml, show_j
 
 app_name = 'main'
 
-
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product/', create_product, name='create_product'),
